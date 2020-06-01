e3a06f69495dc656059f03660c38d7933c76173c
Rodrigo Labrador <tangerinecoder@gmail.com>
2020-06-01 20:40:31 +0200

    Creates Makefile

---
+++ autoMakefile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+find . -name "*.c" > files.txt
+
+echo 'SRCS		= ' > file1.txt
+
+x=$(cat file1.txt files.txt)
+echo $x > Makefile
+
+echo '
+OBJS	= $(SRCS:.c=.o)
+
+NAME	= 	OUTPUT
+
+FLAGS	= -g
+
+GCC = GCC
+
+all:	$(NAME)
+
+$(NAME):	$(OBJS)
+				@echo "\033[0;31m[Remove last version...]"
+			@rm -rf result
+				@echo "\033[0;33m[compilation...]"
+			$(GCC) $(OBJS) -o $(NAME)
+
+%.o: %.c
+	$(GCC) $(FLAGS) -c $<  -o $(<:.c=.o)
+
+fclean: clean
+	rm -f $(NAME)
+
+clean : 
+		@echo "\033[0;31m[Deleting Objects...]"
+	$(RM) $(OBJS) *.o 
+
+re:				fclean $(NAME)' >> Makefile
+
+rm files.txt
+rm file1.txt
